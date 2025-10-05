4a7d495fb3d41e45565503f1c3e8a07c2857eff2
malicsuave <reywillardd01@gmail.com>
2025-10-05 12:51:38 +0800

    Fix merge conflicts and clean up duplicate entries in master layouts

--- resources/views/layouts/public/master.blade.php
+++ resources/views/layouts/public/master.blade.php
@@ -14,22 +14,18 @@
     <!-- Fonts and icons -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Inter:300,400,500,600,700,900" />
     <link href="{{ asset('assets/css/nucleo-icons.css') }}" rel="stylesheet" />
-        <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
+    <link href="{{ asset('assets/css/nucleo-svg.css') }}" rel="stylesheet" />
     <!-- Use same FontAwesome as admin for consistency -->
     <link rel="stylesheet" href="{{ asset('adminlte/plugins/fontawesome-free/css/all.min.css') }}">
     <!-- Backup CDN FontAwesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
  
 
     <!-- CSS Files -->
     @stack('styles')
     <link id="pagestyle" href="{{ asset('assets/css/material-kit.css') }}" rel="stylesheet" />
-        @stack('styles')
-    <link id="pagestyle" href="{{ asset('assets/css/material-kit.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/chatbot.css') }}?v={{ time() }}">
-    <style>
     <style>
       .navbar-toggler,
       .navbar-toggler:focus,
